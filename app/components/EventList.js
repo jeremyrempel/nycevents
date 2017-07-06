ee81a35d5782a8326117eb80a2ba6db5c42fd9fd
Jeremy Rempel <jr@idreamincolour.com>
2017-07-06 11:54:41 -0700

    change eventlist to stateless, functional component

--- app/components/EventList.js
+++ app/components/EventList.js
@@ -3,7 +3,7 @@
 /**
  * Created by jrempel on 6/14/17.
  */
-import React, { PureComponent } from "react";
+import React from "react";
 import {
   Separator,
   Content,
@@ -16,64 +16,61 @@ import {
   Icon
 } from "native-base";
 
-export default class EventList extends PureComponent {
-  dates = [];
+let dates = [];
 
-  render() {
+const EventList = props =>
+  <List
+    style={{ backgroundColor: "white" }}
+    dataArray={props.events}
+    renderRow={item =>
+      <Content>
+        {showHeader(item.startdate) &&
+          <Separator bordered>
+            <Text>
+              {item.startdate}
+            </Text>
+          </Separator>}
+
+        <ListItem onPress={() => this.props.onPress(item)}>
+          {showImage(item.image)}
+          <Body>
+            <Text>
+              {item.title}
+            </Text>
+            <Text note>
+              {item.startdate} {item.starttime}
+            </Text>
+            <Text note>
+              {item.location}
+            </Text>
+          </Body>
+          <Right>
+            <Icon name="arrow-forward" />
+          </Right>
+        </ListItem>
+      </Content>}
+  />;
+
+export default EventList;
+
+function showImage(remoteImageUrl) {
+  if (remoteImageUrl && remoteImageUrl.length > 0) {
     return (
-      <List
-        style={{ backgroundColor: "white" }}
-        dataArray={this.props.events}
-        renderRow={item =>
-          <Content>
-            {this.showHeader(item.startdate) &&
-              <Separator bordered>
-                <Text>
-                  {item.startdate}
-                </Text>
-              </Separator>}
-
-            <ListItem onPress={() => this.props.onPress(item)}>
-              {this.showImage(item.image)}
-              <Body>
-                <Text>
-                  {item.title}
-                </Text>
-                <Text note>
-                  {item.startdate} {item.starttime}
-                </Text>
-                <Text note>
-                  {item.location}
-                </Text>
-              </Body>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
-          </Content>}
+      <Thumbnail
+        large
+        source={{ uri: remoteImageUrl.replace("http", "https") }}
       />
     );
+  } else {
+    return <Thumbnail large source={require("../img/park.png")} />;
   }
+}
 
-  showImage(remoteImageUrl) {
-    if (remoteImageUrl.length > 0) {
-      return (
-        <Thumbnail
-          large
-          source={{ uri: remoteImageUrl.replace("http", "https") }}
-        />
-      );
-    } else {
-      return <Thumbnail large source={require("../img/park.png")} />;
-    }
-  }
-
-  showHeader(date) {
-    if (this.dates.indexOf(date) > -1) {
-      return false;
-    } else {
-      this.dates.push(date);
-      return true;
-    }
+function showHeader(date) {
+  if (dates.indexOf(date) > -1) {
+    return false;
+  } else {
+    dates.push(date);
+    return true;
   }
 }
