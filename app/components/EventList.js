d44bf8c7cecbab52e1f6d09e5f1d24ed0049e977
Jeremy Rempel <jr@idreamincolour.com>
2017-07-06 12:12:47 -0700

    fix a navigation bug caused by refactor to stateless component

--- app/components/EventList.js
+++ app/components/EventList.js
@@ -31,7 +31,7 @@ const EventList = props =>
             </Text>
           </Separator>}
 
-        <ListItem onPress={() => this.props.onPress(item)}>
+        <ListItem onPress={() => props.onPress(item)}>
           {showImage(item.image)}
           <Body>
             <Text>
